a5f2f2dff53d90d1e27e483df863c86d50ef87a2
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2026-09-17 22:17:44 -0300

    Aplicativo com a tela de enviar Email

--- app/src/main/java/br/edu/utfpr/trocatela_2026_2/ConfirmarActivity.kt
+++ app/src/main/java/br/edu/utfpr/trocatela_2026_2/ConfirmarActivity.kt
@@ -1,6 +1,9 @@
 package br.edu.utfpr.trocatela_2026_2
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -34,6 +37,18 @@ class ConfirmarActivity : AppCompatActivity() {
         tvCod.text = cod
         tvQtd.text = qtd
         tvValor.text = valor
+
+    }
+
+    fun btSMSOnClick(view: View) {
+        val msg = "Cod: ${tvCod.text} Qtd: ${tvQtd.text} Valor: ${tvValor.text}"
+        val telefone = "sms:+5546999112233"
+
+        val intent = Intent(Intent.ACTION_VIEW )
+        intent.putExtra( "sms_body", msg)
+        intent.setData( Uri.parse(telefone))
+
+        startActivity( intent )
 
     }
 }
